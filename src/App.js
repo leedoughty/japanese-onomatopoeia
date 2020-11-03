adf2e0a748ea413f8c2dcdd79aab706e08878591
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-03 19:48:41 +0000

    Add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import NavBar from "./NavBar";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world!</h1>
+    <div className="app">
+      <NavBar />
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function NavBar() {
+  return (
+    <nav>
+      <h1>Japanese Onomatopoeia</h1>
+      <ul className="nav-links">
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
