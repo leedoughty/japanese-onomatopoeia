efe86047795a27604a1628b0dccc79849e8d431f
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-06 20:29:10 +0000

    Animate kurukuru text

--- src/word-animations/Kurukuru.js
+++ src/word-animations/Kurukuru.js
@@ -1,9 +1,23 @@
+import styled, { keyframes } from "styled-components";
 import "../App.css";
 
 function Gurarigurari() {
+  const Spin = keyframes`
+    0% {
+      transform: rotateY(0deg);
+    }
+    100% {
+      transform: rotateY(360deg);
+    }
+  `;
+
+  const SpinningAnimation = styled.p`
+    font-size: 100px;
+    animation: ${Spin} 1s linear infinite;
+  `;
   return (
     <div>
-      <h1>くるくる</h1>
+      <SpinningAnimation>くるくる</SpinningAnimation>
     </div>
   );
 }
