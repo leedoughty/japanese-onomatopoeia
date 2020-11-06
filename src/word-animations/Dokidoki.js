f03a87570a8594c6f0f1c56c70ad1d2a0d92dcf2
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-06 20:24:53 +0000

    Animate dokidoki text

--- src/word-animations/Dokidoki.js
+++ src/word-animations/Dokidoki.js
@@ -1,9 +1,27 @@
+import styled, { keyframes } from "styled-components";
 import "../App.css";
 
 function Dokidoki() {
+  const Heartbeat = keyframes`
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(2);
+    }
+    100% {
+      transform: scale(1);
+    }
+  `;
+
+  const HeartbeatAnimation = styled.p`
+    animation: ${Heartbeat} 0.4s ease infinite;
+    font-size: 80px;
+  `;
+
   return (
     <div>
-      <h1>ドキドキ</h1>
+      <HeartbeatAnimation>ドキドキ</HeartbeatAnimation>
     </div>
   );
 }
