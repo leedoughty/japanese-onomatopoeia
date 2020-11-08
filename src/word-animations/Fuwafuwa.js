22c62a422f462d5e8a01ba40c62e5bea9c3222e9
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-08 10:34:52 +0000

    Animate fuwafuwa text

--- src/word-animations/Fuwafuwa.js
+++ src/word-animations/Fuwafuwa.js
@@ -1,9 +1,21 @@
+import styled, { keyframes } from "styled-components";
 import "../App.css";
 
 function Fuwafuwa() {
+  const Floating = keyframes`
+    50% {
+      transform: translateY(20%);
+    }
+  `;
+
+  const FloatingAnimation = styled.p`
+    animation: ${Floating} 1.5s linear infinite;
+    font-size: 80px;
+  `;
+
   return (
     <div>
-      <h1>ふわふわ</h1>
+      <FloatingAnimation>ふわふわ</FloatingAnimation>
     </div>
   );
 }
