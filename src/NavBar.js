324216eca8da3bd37b81af432660d4a7211040dc
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-09 20:15:49 +0000

    Update styling for nav

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,10 +4,10 @@ function NavBar() {
   return (
     <nav>
       <Link aria-label="home" className="nav-link" to="/">
-        <h1>onomato</h1>
+        <h1 className="nav-item">onomato</h1>
       </Link>
       <Link aria-label="about" className="nav-link" to="/about">
-        <h1>about</h1>
+        <h1 className="nav-item">about</h1>
       </Link>
     </nav>
   );
