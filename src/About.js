662814029ff706e98607852c2af59baa7b73929f
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-16 18:02:21 +0000

    Add initial about page text

--- src/About.js
+++ src/About.js
@@ -2,13 +2,14 @@ function About() {
   return (
     <div className="about-container">
       <p className="about-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Onomatopoeia words are use to represent sounds in a language. In
+        Japanese, onomatopoeia play an important role in expressing sound,
+        emotion, movement, feelings, states and much more, and are frequently
+        used in everyday speech and writing.
+      </p>
+      <p className="about-text">
+        This app is both a learning tool and a celebration of these wonderful
+        features of the Japanese language. I hope you enjoy!
       </p>
     </div>
   );
