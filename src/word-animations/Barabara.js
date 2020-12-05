a0d8a4f229d96657b6a2af15e23386fefc912e98
Lee Doughty <leedoughtydesign@gmail.com>
2020-12-05 11:04:55 +0000

    Make barabara text draggable

--- src/word-animations/Barabara.js
+++ src/word-animations/Barabara.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Draggable from "react-draggable";
 
 const BarabaraContainer = styled.div`
   margin: 10px 0 10px 0;
@@ -11,33 +12,36 @@ const BarabaraText = styled.p`
   position: absolute;
   left: ${(props) => props.pos}%;
   font-size: 100px;
-  transform: translate(${(props) => props.x}px, ${(props) => props.y}px);
   color: ${(props) => props.color};
 `;
 
 function Barabara() {
-  function generateX() {
-    return Math.floor(Math.random() * 60);
-  }
-
   function generateY() {
-    return Math.floor(Math.random() * 100);
+    return Math.floor(Math.random() * 150);
   }
 
   return (
     <BarabaraContainer aria-hidden="true">
-      <BarabaraText color="#248cd8" x={generateX()} y={generateY()} pos={10}>
-        バ
-      </BarabaraText>
-      <BarabaraText color="#e6412e" x={generateX()} y={generateY()} pos={30}>
-        ラ
-      </BarabaraText>
-      <BarabaraText color="#e37203" x={generateX()} y={generateY()} pos={50}>
-        バ
-      </BarabaraText>
-      <BarabaraText color="#f0bd00" x={generateX()} y={generateY()} pos={70}>
-        ラ
-      </BarabaraText>
+      <Draggable defaultPosition={{ x: 0, y: generateY() }}>
+        <BarabaraText color="#248cd8" pos={10}>
+          バ
+        </BarabaraText>
+      </Draggable>
+      <Draggable defaultPosition={{ x: 0, y: generateY() }}>
+        <BarabaraText color="#e6412e" pos={30}>
+          ラ
+        </BarabaraText>
+      </Draggable>
+      <Draggable defaultPosition={{ x: 0, y: generateY() }}>
+        <BarabaraText color="#e37203" pos={50}>
+          バ
+        </BarabaraText>
+      </Draggable>
+      <Draggable defaultPosition={{ x: 0, y: generateY() }}>
+        <BarabaraText color="#f0bd00" pos={70}>
+          ラ
+        </BarabaraText>
+      </Draggable>
     </BarabaraContainer>
   );
 }
