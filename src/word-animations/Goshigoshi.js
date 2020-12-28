5aad5dead8df48c3560bc7a7a0d68f60d572a146
Lee Doughty <leedoughtydesign@gmail.com>
2020-12-28 13:31:09 +0000

    Add drawing brush to goshigoshi word component

--- src/word-animations/Goshigoshi.js
+++ src/word-animations/Goshigoshi.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 function Goshigoshi() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 10;
+    contextRef.current = context;
+  }, []);
+
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
   return (
-    <div>
+    <div style={{ border: "1px solid blue", height: "50vh" }}>
       <h1>ゴシゴシ</h1>
+      <canvas
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
     </div>
   );
 }
