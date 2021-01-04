df3566e0431cd78149efd49cf4c58c21bc85257f
Lee Doughty <leedoughtydesign@gmail.com>
2021-01-04 13:35:37 +0000

    Add ascii sparkles to kirakira component

--- src/word-animations/Kirakira.js
+++ src/word-animations/Kirakira.js
@@ -1,10 +1,31 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const KirakiraContainer = styled.div`
+  margin: 10px 0 10px 0;
+  width: 90vw;
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const KirakiraText = styled.h1`
+  font-size: 60px;
+`;
+
+const Sparkles = styled.span`
+  font-size: 50px;
+  position: absolute;
+  color: #ffc600;
+`;
 
 function Kirakira() {
   return (
-    <div>
-      <h1>きらきら</h1>
-    </div>
+    <KirakiraContainer>
+      <KirakiraText>きらきら</KirakiraText>
+      <Sparkles aria-hidden="true">✧･ﾟ: *✧･ﾟ:*:･ﾟ✧*:･ﾟ✧･ﾟ:</Sparkles>
+    </KirakiraContainer>
   );
 }
 
