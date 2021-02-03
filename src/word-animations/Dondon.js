2805b151d6add0df2a5cf0bc50c0a04a8769fdd8
Lee Doughty <leedoughtydesign@gmail.com>
2021-02-03 18:47:09 +0000

    Update dondon layout style

--- src/word-animations/Dondon.js
+++ src/word-animations/Dondon.js
@@ -1,10 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const DondonContainer = styled.div`
+  margin: 10px 0 10px 0;
+  width: 90vw;
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const DondonText = styled.p`
+  font-size: 80px;
+  @media (max-width: 768px) {
+    font-size: 40px;
+  }
+`;
 
 function Dondon() {
   return (
-    <div>
-      <h1>ドンドン</h1>
-    </div>
+    <DondonContainer>
+      <DondonText>ドンドン</DondonText>
+      <DondonText>ドンドン</DondonText>
+      <DondonText>ドンドン</DondonText>
+      <DondonText>ドンドン</DondonText>
+    </DondonContainer>
   );
 }
 
