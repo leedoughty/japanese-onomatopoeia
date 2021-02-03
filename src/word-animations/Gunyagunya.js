47bdad4e82f3c5a31338dfbf9db2c536093acd46
Lee Doughty <leedoughtydesign@gmail.com>
2021-02-03 18:41:58 +0000

    Add gunyagunya graphic

--- src/word-animations/Gunyagunya.js
+++ src/word-animations/Gunyagunya.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import GunyagunyaText from "./images/squishy.svg";
+
+const GunyagunyaContainer = styled.div`
+  margin: 10px 0 10px 0;
+  background-color: #f1c410;
+  width: 90vw;
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const GunyagunyaImage = styled.img`
+  width: 400px;
+  height: auto;
+`;
 
 function Gunyagunya() {
   return (
-    <div>
-      <h1>ぐにゃぐにゃ</h1>
-    </div>
+    <GunyagunyaContainer>
+      <GunyagunyaImage src={GunyagunyaText} alt="Squishy" />
+    </GunyagunyaContainer>
   );
 }
 
