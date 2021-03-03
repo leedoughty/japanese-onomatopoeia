6daa22d76519b3e479839d1da9eea5d5393d27c5
Lee Doughty <leedoughtydesign@gmail.com>
2021-03-03 16:46:13 +0000

    Update spacing of word and sentence components

--- src/Word.js
+++ src/Word.js
@@ -1,13 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const WordContainer = styled.section`
+  width: 90vw;
+  margin: auto;
+  padding: 20px 0 20px 0;
+`;
 
 function Word({ id, word, meaning }) {
   return (
-    <section>
+    <WordContainer>
       <span className="japanese-word">{word}</span>
       <span className="romaji-word">{id}</span>
       <br />
       <span className="english-word">{meaning}</span>
-    </section>
+    </WordContainer>
   );
 }
 
