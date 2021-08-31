483ae2e8c628f66145091f64e6267d7f5f2e9aff
Lee Doughty <leedoughtydesign@gmail.com>
2021-08-31 18:28:11 +0100

    Update components text styling

--- src/word-animations/Pakupaku.js
+++ src/word-animations/Pakupaku.js
@@ -1,12 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import WordAnimationBackground from "./WordAnimationBackground";
 import WordAnimationContainer from "./WordAnimationContainer";
 
+const PakupakuText = styled.p`
+  font-size: 80px;
+  @media (max-width: 768px) {
+    font-size: 40px;
+  }
+`;
+
 function Pakupaku() {
   return (
-    <WordAnimationBackground>
+    <WordAnimationBackground navBarColour="black">
       <WordAnimationContainer>
-        <span>ぱくぱく</span>
+        <PakupakuText>ぱくぱく</PakupakuText>
       </WordAnimationContainer>
     </WordAnimationBackground>
   );
