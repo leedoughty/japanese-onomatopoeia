0696016d23c5dc50cf89d1b48045148871a4e2f9
Lee Doughty <leedoughtydesign@gmail.com>
2021-08-31 19:55:39 +0100

    Add katakata animation

--- src/word-animations/Katakata.js
+++ src/word-animations/Katakata.js
@@ -4,9 +4,47 @@ import WordAnimationBackground from "./WordAnimationBackground";
 import WordAnimationContainer from "./WordAnimationContainer";
 
 const KatakataText = styled.p`
+  --typingSpeed: 3s;
+
   font-size: 80px;
+  position: relative;
+  width: max-content;
+
   @media (max-width: 768px) {
     font-size: 40px;
+  }
+
+  &:before,
+  &:after {
+    position: absolute;
+    content: "";
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  &:before {
+    background: #f5f3ef;
+    animation: typing var(--typingSpeed) steps(12) 1s forwards;
+  }
+
+  &:after {
+    width: 0.125em;
+    background: rgb(150, 150, 150);
+    animation: typing var(--typingSpeed) steps(12) 1s forwards, blink 500ms steps(12) infinite;
+  }
+
+  @keyframes typing {
+    to {
+      left: 100%;
+    }
+  }
+
+  @keyframes blink {
+    to {
+      background: transparent;
+    }
   }
 `;
 
@@ -14,7 +52,7 @@ function Katakata() {
   return (
     <WordAnimationBackground navBarColour="black">
       <WordAnimationContainer>
-        <KatakataText>カタカタ</KatakataText>
+        <KatakataText>カタカタカタカタカタカタ</KatakataText>
       </WordAnimationContainer>
     </WordAnimationBackground>
   );
