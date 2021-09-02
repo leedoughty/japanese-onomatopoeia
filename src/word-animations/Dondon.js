c9b1f8c10e41651bfc62660ae0e9992d9dd09a02
Lee Doughty <leedoughtydesign@gmail.com>
2021-09-02 18:33:49 +0100

    Add dondon animation

--- src/word-animations/Dondon.js
+++ src/word-animations/Dondon.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import WordAnimationBackground from "./WordAnimationBackground";
 import WordAnimationContainer from "./WordAnimationContainer";
@@ -6,18 +6,26 @@ import WordAnimationContainer from "./WordAnimationContainer";
 const DondonText = styled.p`
   font-size: 80px;
   @media (max-width: 768px) {
-    font-size: 40px;
+    font-size: 20px;
   }
 `;
 
 function Dondon() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText((text) => {
+        return text + "ドン";
+      });
+    }, 800);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <WordAnimationBackground navBarColour="black">
-      <WordAnimationContainer>
-        <DondonText>ドンドン</DondonText>
-        <DondonText>ドンドン</DondonText>
-        <DondonText>ドンドン</DondonText>
-        <DondonText>ドンドン</DondonText>
+      <WordAnimationContainer aria-hidden="true">
+        <DondonText>{text}</DondonText>
       </WordAnimationContainer>
     </WordAnimationBackground>
   );
